9e4b91ca4f04347f268931e503dfab9c8fa9a164
Krish-Das <krishdasatgames@gmail.com>
2025-01-10 14:19:36 +0530

    add zsh config

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+# ▄▀█ █░░ █ ▄▀█ █▀ █▀▀ █▀
+# █▀█ █▄▄ █ █▀█ ▄█ ██▄ ▄█
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# settings for common commands
+alias nv="nvim"
+alias ya="flatpak run io.github.sxyazi.yazi" # "yazi" # for Arch-land 🙂
+alias fetch="fastfetch"
+alias lg="lazygit"
+alias cp="cp -iv"
+alias mv="mv -iv"
+alias rm="rm -vI"
+alias bc="bc -ql"
+alias mkd="mkdir -pv"
+alias ls="eza --icons --group-directories-first"
+alias tree="eza -T"
+alias bat="bat -p"
+alias c="wl-copy"
+alias open="$EXPLORER"
+# alias c="xclip -selection clipboard"
+# alias lf="lfcd"
+# alias locate="plocate"
+
+alias tp="trash-put"
+alias tpr="trash-restore"
+
+alias s='echo no-sirski!'
+alias H='Hyprland'
+
+alias Trash="cd \$HOME/.local/share/Trash/files"
+# alias Hdd="cd /media/Hdd/" # for Arch-land 🙂
+alias run="cd /run/media/$USER"
+
+# git aliases
+alias galias="bat ~/.config/zsh/aliases.zsh | grep 'alias g' | fzf --cycle" 
+alias g="git"
+alias gst="git status"
+alias gcm="git commit"
+alias gad="git add"
+alias gpl="git pull"
+alias gpu="git push"
+alias gd="git diff"
+alias gch="git checkout"
+alias gnb="git checkout -b"
+alias gac="git add . && git commit"
+alias grs="git restore --staged ."
+alias gre="git restore"
+alias gr="git remote"
+alias gcl="git clone"
+alias glg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold green)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold yellow)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+alias gt="git ls-tree -r master --name-only"
+alias grm="git remote"
+alias gbr="git branch"
+alias gf="git fetch"
+
+# easier to read disk
+alias df='df -h'     # human-readable sizes
+alias free='free -m' # show sizes in MB
+
+# get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4 | head -5'
+
+# get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3 | head -5'
+
+# systemd
+alias mach_list_systemctl="systemctl list-unit-files --state=enabled"
+
+# Check startup time of zsh
+alias zsh_time="time zsh -i -l -c exit"
