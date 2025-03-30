1b5300aafa810814a673e7bee33e00037ed199e3
Krish-Das <krishdasatgames@gmail.com>
2025-03-30 11:41:31 +0530

    feat: add launch script

---
+++ .local/bin/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+silent() {
+  if command -v "$1" >/dev/null; then
+    "$@" >/dev/null 2>&1 &
+    pid=$!
+    printf "\033[32mRan %s in background silently (PID: %d).\033[0m\n" "$*" $pid
+  else
+    printf "\033[31mError: %s is not a valid command.\033[0m\n" "$1"
+    return 127
+  fi
+}
+
+silent ghostty &&
+  silent firefox &&
+  silent flatpak run com.google.Chrome &&
+  silent flatpak run app.zen_browser.zen &&
+  silent ghostty
