4c0f463a15b86d4a8f5b5660d57fc1eff339ab3e
Krish-Das <krishdasatgames@gmail.com>
2025-04-03 15:29:29 +0530

    feat: add `Bd` in the custom usercommand

--- lua/user/usercommand.lua
+++ lua/user/usercommand.lua
@@ -1,18 +1,20 @@
 -- List of command pairs (uppercase -> lowercase)
 local commands = {
-    ['W'] = 'w',
-    ['Wq'] = 'wq',
-    ['WQ'] = 'wq',
-    ['Wa'] = 'wa',
-    ['WA'] = 'wa',
-    ['Wqa'] = 'wqa',
-    ['WQa'] = 'wqa',
-    ['WQA'] = 'wqa'
+  ["W"] = "w",
+  ["Wq"] = "wq",
+  ["WQ"] = "wq",
+  ["Wa"] = "wa",
+  ["WA"] = "wa",
+  ["Wqa"] = "wqa",
+  ["WQa"] = "wqa",
+  ["WQA"] = "wqa",
+  ["Bd"] = "bd",
+  ["Bda"] = "bda",
 }
 
 -- Create command aliases
 for uppercase, lowercase in pairs(commands) do
-    vim.api.nvim_create_user_command(uppercase, lowercase, {
-        desc = string.format("Write command alias for :%s", lowercase)
-    })
+  vim.api.nvim_create_user_command(uppercase, lowercase, {
+    desc = string.format("Write command alias for :%s", lowercase),
+  })
 end
