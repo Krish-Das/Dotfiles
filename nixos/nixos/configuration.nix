7d8468f3fab64119862a4fb78aacb71a21325f3c
Krish-Das <krishdasatgames@gmail.com>
2025-04-29 14:02:00 +0530

    fix: disable hardware cursor

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -46,7 +46,7 @@
     xwayland.enable = true;
   };
   environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "0";
+    WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
   };
   hardware = {
