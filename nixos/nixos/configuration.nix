a09e1c9547981bb30be89b43b53766ede9d768da
Krish-Das <krishdasatgames@gmail.com>
2025-05-01 12:33:17 +0530

    feat: add osprober and reduce timeout duration

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -15,12 +15,16 @@
   nix.settings.experimental-features = ["nix-command"];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    timeout = 1;
+  };
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     configurationLimit = 8;
     device = "nodev";
+    useOSProber = true;
   };
 
   system.name = "didact";
