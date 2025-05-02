3b6a9a06628e868f21976891e60aa00e2800675b
Krish-Das <krishdasatgames@gmail.com>
2025-05-02 13:54:58 +0530

    feat: add treesitter support

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
+  opts = {
+    ensure_installed = {
+      "bash", "css", "javascript", "diff", "html", "lua",
+      "luadoc", "markdown", "markdown_inline", "query",
+      "vim", "vimdoc", "json",
+      "tsx", "typescript", "nix"
+    },
+    auto_install = true,
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>", -- set to `false` to disable one of the mappings
+        node_incremental = "<CR>",
+        scope_incremental = "grc",
+        node_decremental = "<BS>",
+      },
+    },
+  }
+}
+
+return M
