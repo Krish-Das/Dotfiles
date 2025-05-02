0beb3b70836ed6aafbe858a93b2884b51bac47f9
Krish-Das <krishdasatgames@gmail.com>
2025-05-02 11:53:48 +0530

    feat: enable the plugin dir in lazy
    - Enable the plugin dir in lazy
    - Installed catpuccin theme

---
+++ lua/plugins/catpuccin-theme.lua
@@ -0,0 +1,15 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = true,
+
+  opts = {
+    transparent_background = false,
+    styles = {
+      comments = { "italic" },
+      conditionals = { "italic" },
+      -- functions = { "bold" },
+      -- types = { "bold" },
+    },
+  },
+}
