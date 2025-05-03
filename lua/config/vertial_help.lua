2e5438c159c9609026de5ca6db7b18bcc626c846
Krish-Das <krishdasatgames@gmail.com>
2025-05-03 12:34:56 +0530

    feat: add vertical help function

---
+++ lua/config/vertial_help.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("vertical_help", { clear = true }),
+  pattern = "*.txt",
+  callback = function()
+    if vim.bo.buftype == "help" then
+      vim.cmd.wincmd "L"
+      vim.cmd.wincmd "="
+    end
+  end,
+})
