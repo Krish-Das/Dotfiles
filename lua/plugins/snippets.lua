ab77f972a361f8dd974fc502cc50cb567255bfd0
Krish-Das <krishdasatgames@gmail.com>
2025-05-23 01:05:45 +0530

    chore: disable the snippets for my sanity!

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -4,29 +4,26 @@ return {
   },
   {
     "L3MON4D3/LuaSnip",
-    version = "2.*",
-    build = (function()
-      if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-        return
-      end
-      return "make install_jsregexp"
-    end)(),
-    dependencies = {
-      {
-        "rafamadriz/friendly-snippets",
-        config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-      },
-      {
-        "mlaursen/vim-react-snippets",
-        config = function()
-          require("vim-react-snippets").lazy_load()
-          local config = require("vim-react-snippets.config")
-          config.readonly_props = false
-        end,
-      },
+    lazy = true,
+    dependencies = {},
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
     },
-    opts = {},
   },
 }
+
+-- {
+-- "rafamadriz/friendly-snippets",
+--   config = function()
+--     require("luasnip.loaders.from_vscode").lazy_load()
+--   end
+-- }
+-- {
+--   "mlaursen/vim-react-snippets",
+--   config = function()
+--     require("vim-react-snippets").lazy_load()
+--     local config = require("vim-react-snippets.config")
+--     config.readonly_props = false
+--   end,
+-- }
