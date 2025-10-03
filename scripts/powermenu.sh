edb487cf2f81ff3dee0c9981cfcb19d2775dacd6
Krish-Das <krishdasatgames@gmail.com>
2025-10-03 17:56:48 +0530

    chore: update UI icons (power menu) and Zed drag‑and‑drop config

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -5,21 +5,21 @@
 CHOSEN=$(printf "Do nothing\nReboot\nShutdown\nQuit Hyprland" | tofi --config "$HOME"/.config/tofi/powermenu-config)
 
 case "$CHOSEN" in
-	"Reboot")
-		ACTION="reboot"
-		PROMPT_TEXT="󰜉"
-		;;
-	"Shutdown")
-		ACTION="poweroff"
-		PROMPT_TEXT=""
-		;;
-	"Quit Hyprland")
-		ACTION="hyprctl dispatch exit"
-		PROMPT_TEXT=""
-		;;
-	*)
-		exit 1
-		;;
+"Reboot")
+  ACTION="reboot"
+  PROMPT_TEXT="􀅈"
+  ;;
+"Shutdown")
+  ACTION="poweroff"
+  PROMPT_TEXT="􀆨"
+  ;;
+"Quit Hyprland")
+  ACTION="hyprctl dispatch exit"
+  PROMPT_TEXT="􀻵"
+  ;;
+*)
+  exit 1
+  ;;
 esac
 
 CONFIRM=$(printf "No\nYes" | tofi --prompt-text="$PROMPT_TEXT" --config "$HOME"/.config/tofi/powermenu-config)
