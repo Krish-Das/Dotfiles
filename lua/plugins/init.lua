409199beccc8081063925d9b92fe6123db7a8044
Krish-Das <krishdasatgames@gmail.com>
2025-10-30 14:41:30 +0530

    Clean up plugin setup and improve LSP configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,27 +1,16 @@
 local map = require("config.keymap_utils")
 
-vim.opt.winborder = "rounded"
-
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/vague2k/vague.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/echasnovski/mini.pick" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
 require("mini.pick").setup()
 require("oil").setup()
 map("<leader>e", "<cmd>Pick files<CR>")
 map("<leader>-", "<cmd>Oil<CR>")
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if clent:supports_method('textDocument/completion')then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end
-})
 
 -- Set colorscheme
 vim.cmd("colorscheme vague")
@@ -30,4 +19,13 @@ vim.cmd(":hi statusline guibg=NONE")
 map("<leader>rr", ":update<CR>:source<CR>", "Yank current line to system clipboard")
 
 vim.lsp.enable({ "lua_ls" })
-map("<leader>fm", ":= vim.lsp.buf.format()<CR>", "Format current buffer")
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true)
+      }
+    }
+  }
+})
+map("<leader>fm", function() vim.lsp.buf.format() end)
