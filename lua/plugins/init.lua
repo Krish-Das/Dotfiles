8bcd7097ab76731c9d4d64e73efd75ed91788451
Krish-Das <krishdasatgames@gmail.com>
2025-10-30 16:21:56 +0530

    Add descriptions to keybind mappings for clarity

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,9 +30,9 @@ vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
 -- Some keybinds
-map("<leader>e", "<cmd>Pick files<CR>")
-map("<leader>-", "<cmd>Oil<CR>")
+map("<leader>e", "<cmd>Pick files<CR>", "Open file picker")
+map("<leader>-", "<cmd>Oil<CR>", "Open directory in Oil buffer")
 map("<leader>rr", ":update<CR>:source<CR>", "Yank current line to system clipboard")
 map("<leader>fm", function()
   vim.lsp.buf.format()
-end)
+end, "Format current buffer via LSP")
