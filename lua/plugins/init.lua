bf9bc1123166f0e1364b37dcbf7311e530a2ba3a
Krish-Das <krishdasatgames@gmail.com>
2025-10-30 14:54:27 +0530

    Add Mason and Treesitter plugins with initial setup and configs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,10 +5,22 @@ vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/echasnovski/mini.pick" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
 require("mini.pick").setup()
 require("oil").setup()
+require("mason").setup()
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "typescript", "javascript" },
+  auto_install = false,
+  ignore_install = {},
+  modules = {},
+  sync_install = false,
+  highlight = { enable = true }
+})
+
 map("<leader>e", "<cmd>Pick files<CR>")
 map("<leader>-", "<cmd>Oil<CR>")
 
@@ -18,7 +30,7 @@ vim.cmd(":hi statusline guibg=NONE")
 
 map("<leader>rr", ":update<CR>:source<CR>", "Yank current line to system clipboard")
 
-vim.lsp.enable({ "lua_ls" })
+vim.lsp.enable({ "lua_ls", "biome" })
 vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
@@ -28,4 +40,6 @@ vim.lsp.config("lua_ls", {
     }
   }
 })
+
+vim.lsp.config("biome", {})
 map("<leader>fm", function() vim.lsp.buf.format() end)
