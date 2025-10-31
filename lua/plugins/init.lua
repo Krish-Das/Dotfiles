84a6ef963e21be961bb68b8afd7ac965dec1e90c
Krish-Das <krishdasatgames@gmail.com>
2025-10-31 23:27:35 +0530

    Add Oil plugin and keybindings for directory management
    Implement the Oil plugin to manage files and directories. Set up
    keybindings to toggle the directory view efficiently.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,10 +2,9 @@ require("plugins.lsp")
 require("config.treesitter")
 require("config.formatter")
 require("plugins.mini_nvim")
+require("plugins.oil")
 
 vim.pack.add({
-  { src = "https://github.com/stevearc/oil.nvim" },
-  -- themes
   { src = "https://github.com/marko-cerovac/material.nvim" },
 })
 
@@ -15,8 +14,3 @@ vim.cmd.highlight("statusline guibg=NONE")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-
--- Some keybinds
-require("oil").setup()
-local map = require("config.keymap_utils")
-map("<leader>e", "<cmd>Oil<CR>", "Open directory in Oil buffer")
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,15 @@
+vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
+
+local map = require("config.keymap_utils")
+local oil = require("oil")
+
+oil.setup()
+
+-- Keybind to toggle oil
+map("<leader>e", function()
+  if vim.bo.filetype == "oil" then
+    oil.close()
+  else
+    oil.open()
+  end
+end, "Toggle Oil")
