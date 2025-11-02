7b7a9613e20170da70c7c65c7cf779669888b070
Krish-Das <krishdasatgames@gmail.com>
2025-11-02 13:01:30 +0530

    Add mappings to pick unstaged and staged files

--- lua/plugins/mini_nvim.lua
+++ lua/plugins/mini_nvim.lua
@@ -10,3 +10,27 @@ map("<leader>ff", "<cmd>Pick files<CR>", "[F]ind [F]iles")
 map("<leader><leader>", "<cmd>Pick buffers<CR>", "[ ] Find existing buffers")
 map("<leader>fg", "<cmd>Pick grep_live<CR>", "[F]ind by [G]rep")
 map("<leader>fh", "<cmd>Pick help<CR>", "[F]ind [H]elp")
+
+-- Git unstaged (modified, tracked)
+map("<leader>gu", function()
+  MiniPick.start({
+    source = {
+      items = function()
+        return vim.fn.systemlist("git diff --name-only --relative")
+      end,
+      name = "Git unstaged",
+    },
+  })
+end, "[G]it [U]nstaged")
+
+-- Git staged
+map("<leader>gd", function()
+  MiniPick.start({
+    source = {
+      items = function()
+        return vim.fn.systemlist("git diff --cached --name-only --relative")
+      end,
+      name = "Git staged",
+    },
+  })
+end, "[G]it stage[D]")
