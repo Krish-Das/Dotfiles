620a0d74e86f585a06bc8424a9142fd333e6df8a
Krish-Das <krishdasatgames@gmail.com>
2025-11-07 17:15:59 +0530

    fix(colorscheme): change colorscheme to catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 local lze = require("plugin-helper")
 lze.add({
   {
-    src = "https://github.com/dracula/vim",
-    name = "dracula",
+    src = "https://github.com/catppuccin/nvim",
+    name = "catppuccin",
     data = {
       event = "VimEnter",
       after = function()
-        vim.cmd.colorscheme("dracula")
+        vim.cmd.colorscheme("catppuccin-frappe")
+        vim.cmd.highlight("statusline guibg=NONE")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
       end,
     },
   },
