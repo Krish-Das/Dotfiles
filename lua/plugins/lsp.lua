264f5f695512d8d8e5adddb05eb4185440ffba30
Krish-Das <krishdasatgames@gmail.com>
2025-11-14 14:51:13 +0530

    setup formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -86,6 +86,38 @@ local lsp = {
   end,
 }
 
-local formatter = {}
+local formatter = {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      nix = { "alejandra" },
+      yaml = { "biome" },
+      toml = { "biome" },
+      json = { "biome" },
+      jsonc = { "biome" },
+      css = { "biome" },
+      typescript = { "biome" },
+      javascript = { "biome" },
+      javascriptreact = { "biome" },
+      typescriptreact = { "biome" },
+      html = { "prettierd", "prettier", stop_after_first = true }, -- Biome is not working on this
+      markdown = { "prettierd", "prettier", stop_after_first = true }, -- No support for Biome
+    },
+    notify_on_error = false,
+    format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+  },
+  keys = {
+    {
+      "<leader>fm",
+      function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
+      end,
+      desc = "[F]or[M]at buffer",
+    },
+  },
+}
 
 return { lsp, formatter }
