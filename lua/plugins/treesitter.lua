ecf0bc4c1f092f4a2b89a369a1048ddc37a9c128
Krish-Das <krishdasatgames@gmail.com>
2025-11-14 14:12:30 +0530

    install treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,57 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  init = function()
+    vim.o.foldmethod = "expr"
+    vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+  end,
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
+  opts = {
+    ensure_installed = {
+      "diff",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "json",
+      "jsonc",
+      "typescript",
+      "javascript",
+      "tsx",
+      "css",
+      "html",
+      "bash",
+      "nix",
+      "vim",
+      "vimdoc",
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+      --
+      -- disable slow treesitter highlight for large files
+      -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#modules
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
+    indent = { enable = true },
+    auto_install = false,
+    sync_install = false,
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        scope_incremental = "grc",
+        node_decremental = "<BS>",
+      },
+    },
+  },
+}
