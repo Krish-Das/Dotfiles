acffc22e6b3b26387172a9d198130afb1c8231c6
Krish-Das <krishdasatgames@gmail.com>
2025-11-15 17:27:04 +0530

    fix(scripts): update screenshot script to support monitor selection
    Added functionality to select specific monitors for screenshots.

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,18 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# Get the current date and time
 time_stamp=$(date +"%d%m%y-%H%M%S%3N")
 
 # File path and name
 file_path="$HOME"/Pictures/Screenshots
-file_name=Screenshot-"$time_stamp"
+file_name=screenshot-"$time_stamp"
 file_ext=".png"
 
 # Use awk to extract the simplified path
 simple_path=$(echo "$file_path" | awk -F"$HOME/" '{print $2}')
 
-grim "$file_path"/"$file_name""$file_ext"
+# Monitor Selection
+selected_monitors=$(hyprctl monitors | awk '/^Monitor/ {print $2}' | tofi)
+[ -z "$selected_monitors" ] && { exit 0; }
+
 notify-send "Screenshot saved" "in $simple_path" -t 1000
+grim -o "$selected_monitors" "$file_path"/"$file_name""$file_ext"
 
 # Selected region
 # grim -g "$(slurp -c #FF0000FF -d)"
