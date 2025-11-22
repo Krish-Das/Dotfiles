648442b3f916142b59f4fd7565a8d317a480581a
Krish-Das <krishdasatgames@gmail.com>
2025-11-22 21:04:44 +0530

    chore(nixos): remove comments

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -100,11 +100,6 @@
 
   fonts.packages = with pkgs; [jetbrains-mono];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
