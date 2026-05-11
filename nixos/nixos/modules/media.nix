9c3d5de931bf085cf0856af069f431559511efe1
Krish-Das <krishdasatgames@gmail.com>
2026-05-11 06:29:06 +0000

    fix(media): enable ACP auto-profile for ALSA devices
    Configure Wireplumber to use ALSA Control Panel with automatic
    profile switching for both integrated and discrete audio devices.

--- nixos/nixos/modules/media.nix
+++ nixos/nixos/modules/media.nix
@@ -8,6 +8,24 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
+    wireplumber.extraConfig."51-disable-pro-audio" = {
+      "monitor.alsa.rules" = [
+        {
+          matches = [{"device.name" = "alsa_card.pci-0000_01_00.1";}];
+          actions.update-props = {
+            "api.alsa.use-acp" = true;
+            "api.acp.auto-profile" = true;
+          };
+        }
+        {
+          matches = [{"device.name" = "alsa_card.pci-0000_00_1b.0";}];
+          actions.update-props = {
+            "api.alsa.use-acp" = true;
+            "api.acp.auto-profile" = true;
+          };
+        }
+      ];
+    };
   };
 
   # Enable media-controles with playerctl
