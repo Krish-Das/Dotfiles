9573c9adadedc773f2b5d5c277e1401cdd5c6905
Krish-Das <krishdasatgames@gmail.com>
2026-05-15 11:16:10 +0000

    chore(config): update shell and mime type defaults
    - Fix background process redirection in silent function
    - Set Helium as default markdown viewer
    - Add MKV video files to MPV default handler
    - Enable smart-case search in ripgrep

--- home/.config/zsh/functions.zsh
+++ home/.config/zsh/functions.zsh
@@ -50,7 +50,7 @@ rp() {
 
 silent() {
   if command -v "$1" >/dev/null; then
-    "$@" &>/dev/null &!
+    "$@" >/dev/null 2>&1 &
     pid=$!
     printf "\033[32mRan %s in background silently (PID: %d).\033[0m\n" "$*" $pid
   else
