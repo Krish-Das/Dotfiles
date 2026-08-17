323a409fd088e4125e55daf1bceb38a32efaef4d
Krish-Das <krishdasatgames@gmail.com>
2026-08-17 06:51:15 +0000

    style(wezterm): adjust font size, padding, and color scheme
    - Reduce font size from 13 to 11 and enable line height adjustment
    - Remove window padding for a more compact layout
    - Prevent window resizing when font size changes
    - Switch color scheme from ChallengerDeep to Tokyo Night
    - Reorganize config for better readability

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -4,30 +4,30 @@ if wezterm.config_builder then
   c = wezterm.config_builder()
 end
 
+c.term = "wezterm"
+
 -- set different shell e.g.: fish
 -- config.default_prog = { "/run/current-system/sw/bin/fish", "--interactive" }
 
-c.term = "wezterm"
-
-c.font_size = 13
--- config.line_height = 1.15
+c.font_size = 11
+c.line_height = 1.0
 c.font = wezterm.font("JetBrainsMono Nerd Font")
 c.harfbuzz_features = { "calt=0", "ss01=1", "zero" }
 c.freetype_load_target = "HorizontalLcd" -- "Light" -- HorizontalLcd
 c.freetype_render_target = "HorizontalLcd"
 c.freetype_load_flags = "NO_HINTING|NO_AUTOHINT"
+c.adjust_window_size_when_changing_font_size = false
 
 c.window_background_opacity = 1
-c.window_padding = { left = 2, right = 1, top = 1, bottom = 1 }
+c.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 c.cursor_thickness = 2
 c.window_close_confirmation = "NeverPrompt"
 
 ---@diagnostic disable-next-line: unused-local
 local schemes = { "Tokyo Night", "ChallengerDeep", "GitHub Dark" }
-c.color_scheme = schemes[2] -- one indexed
+c.color_scheme = schemes[1] -- one indexed
 c.colors = {
-  background = "#030712",
-  -- background = "black",
+  background = "black",
   -- cursor_bg = "#DC322F",
   -- cursor_fg = "#fedad5",
 }
